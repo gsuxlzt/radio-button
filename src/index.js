5d8ef61d6247a389153c1b0010ace8971a85ff66
gsuxlzt <jami.carangan@gmail.com>
2018-08-18 20:38:57 +0800

    :lipstick: add normalize css.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { injectGlobal } from 'styled-components';
 import App from './components/App';
+import { normalize } from './styles';
+
 import registerServiceWorker from './registerServiceWorker';
 
+injectGlobal`
+${normalize}
+`
 render(<App />, document.getElementById('root'));
 registerServiceWorker();
