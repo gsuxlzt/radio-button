a3e28ab87d9464fb1189d6bbc91d9db9ddad4cc2
gsuxlzt <jami.carangan@gmail.com>
2018-08-18 20:29:59 +0800

    :truck: restructure app.

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div>test</div>
+    )
+  }
+}
+
+export default App;
